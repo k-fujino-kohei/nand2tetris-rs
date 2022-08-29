9ddff9a766af55c3a3824f92a27714756fe5f85b
Kohei Fujino <appleblueberrycherry@gmail.com>
2022-08-30 00:28:05 +0900

    :feat: impl full_adder

---
+++ src/ch_2/full_adder.rs
@@ -0,0 +1,31 @@
+use super::{harf_adder::harf_adder, AdderOutput};
+use crate::ch_1::{or, Bit};
+
+pub fn full_adder(a: Bit, b: Bit, c: Bit) -> AdderOutput {
+    let ab = harf_adder(a, b);
+    let abc = harf_adder(ab.sum, c);
+    AdderOutput {
+        carry: or(ab.carry, abc.carry),
+        sum: abc.sum,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[rstest(input, output,
+        case((0, 0, 0), (0, 0)),
+        case((0, 0, 1), (0, 1)),
+        case((0, 1, 0), (0, 1)),
+        case((0, 1, 1), (1, 0)),
+        case((1, 0, 0), (0, 1)),
+        case((1, 0, 1), (1, 0)),
+        case((1, 1, 0), (1, 0)),
+        case((1, 1, 1), (1, 1))
+    )]
+    fn test_harf_adder(input: (Bit, Bit, Bit), output: (Bit, Bit)) {
+        assert_eq!(full_adder(input.0, input.1, input.2), output.into());
+    }
+}
--- src/ch_2/mod.rs
+++ src/ch_2/mod.rs
@@ -1,5 +1,6 @@
 use crate::ch_1::Bit;
 
+mod full_adder;
 mod harf_adder;
 
 #[derive(Debug, PartialEq, Eq)]
